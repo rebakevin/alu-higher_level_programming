b717615a9323a0449314ab2260cac53b1809aa6f
rebakevin <r.kevin@alustudent.com>
2025-06-27 19:32:18 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 
 if __name__ == "__main__":
     import sys
-    
+
     argv = sys.argv[1:]
     num_args = len(argv)
-    
+
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
