b4c60167b665c22d967e6742feab3968fb509f8a
rebakevin <r.kevin@alustudent.com>
2025-06-27 10:31:22 -0700

    new exercise

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    import sys
+    
+    argv = sys.argv[1:]
+    num_args = len(argv)
+    
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[0]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i in range(num_args):
+            print("{}: {}".format(i + 1, argv[i]))
