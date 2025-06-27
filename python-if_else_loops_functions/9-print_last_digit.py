71fe70375bf1c6e5347421df53133c3111c69891
rebakevin <r.kevin@alustudent.com>
2025-06-27 19:04:37 +0200

    Update 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
-def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit, end="")
-    return last_digit
+
+def uppercase(str):
+    result = ""
+    for char in str:
+        if ord(char) >= 97 and \
+           ord(char) <= 122:  # Check if lowercase (a-z)
+            result += chr(ord(char) - 32)  # Convert to uppercase
+        else:
+            result += char  # Keep non-lowercase characters unchanged
+    print("{}".format(result))
