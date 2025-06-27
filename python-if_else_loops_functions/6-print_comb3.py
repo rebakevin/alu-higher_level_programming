37c54e97bf680da411ceb6a3173b89fc9c51f8d8
rebakevin <r.kevin@alustudent.com>
2025-06-27 18:48:50 +0200

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print(f"{i:02d}{j:02d}")
+            print("{:02d}{:02d}".format(i, j))
         else:
-            print(f"{i:02d}{j:02d}", end=", ")
+            print("{:02d}{:02d}".format(i, j), end=", ")
