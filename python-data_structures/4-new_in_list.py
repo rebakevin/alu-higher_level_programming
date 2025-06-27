b48734532b57e99d8777a407be1f69d430f36169
rebakevin <r.kevin@alustudent.com>
2025-06-27 10:56:33 -0700

    new exercise

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    
+    # Create a copy of the original list
+    new_list = my_list[:]
+    
+    # Replace the element at the given index
+    new_list[idx] = element
+    
+    return new_list
+
