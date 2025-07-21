60381c9f50a301cfb11ce4e2ab3a558f5fbc5401
Rebakure <kevinrebakure@gmail.com>
2025-07-21 11:54:49 +0200

    sql more queries

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Query that lists all privileges of the MySQL users user_0d_1 and user_0d_
+
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Query that lists all cities contained in the database hbtn_0d_usa.
+
+SELECT C.id, C.name, S.name 
+FROM cities AS C
+JOIN states AS S ON S.id = C.state_id
+ORDER BY C.id;
